e5489c97d055ba48b9adecb1356c8a9c2c8dbeab
Christopher Mellinger <chme2908@login04.rc.colorado.edu>
2017-09-25 09:54:38 -0600

    Log changes to the bash script.

--- rxx031a_Compute.sh
+++ rxx031a_Compute.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --partition shas
-#SBATCH --job-name 32-Iteration-Run
-#SBATCH --time 01:00:00
-#SBATCH --nodes 1
+#SBATCH --job-name 36-Iteration-Run
+#SBATCH --time 05:00:00
+#SBATCH --nodes 3
 #SBATCH --ntasks-per-node 24
 #SBATCH --qos normal
-#SBATCH --output 32-job_%j.out
+#SBATCH --output test-job_36Iter20Subj_%j.out
 
 ml R
 
-Rscript rxx031a.r
+Rscript rxx031a.r --n.iter=36 --ncores=72  --nsubj=20 --ntarg=8 --nprim=8
