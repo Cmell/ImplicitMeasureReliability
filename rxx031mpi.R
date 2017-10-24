7715bec8aa6273b30c6bfb3018bb4a4552b8567a
Cmell <clmellinger@gmail.com>
2017-10-24 09:50:25 -0600

    Added command line args for variances

--- rxx031mpi.R
+++ rxx031mpi.R
@@ -30,7 +30,9 @@ opts =
     make_option(c("--n.iter"), type="integer", help="number of iterations"),
     make_option(c("--ncores"), type="integer", help="number of cores to use"),
     make_option(c("--ntarg"), type="integer", help="number of targets"),
-    make_option(c("--nsubj"), type="integer", help="number of subjects")
+    make_option(c("--nsubj"), type="integer", help="number of subjects"),
+    make_option(c("--pvarHi"), type="integer", help="variance of primes in 'high' condition"),
+    make_option(c("--pvarLo"), type="integer", help="variance of primes in 'low' condition")
   )
 optParser = OptionParser(option_list = optionList)
 args = parse_args(optParser)
