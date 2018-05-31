44e7fdb9628243893411a4b5678ce86edbe4ebb2
Cmell <clmellinger@gmail.com>
2018-05-31 09:36:12 -0600

    Added let statements before using environment variables set by slurm.

--- rxxArray.sh
+++ rxxArray.sh
@@ -15,8 +15,8 @@ module load R/3.3.0
 module load openmpi/1.10.2
 
 DATE=$(date +%Y-%m-%d_%H.%M.%S)
-ncores=SLURM_NTASKS
-taskId=SLURM_ARRAY_TASK_ID
+let ncores=SLURM_NTASKS
+let taskId=SLURM_ARRAY_TASK_ID
 echo "Number of cores:" $ncores
 ESTFL="Results/est_group${taskId}.RData"
 mpiexec -np $ncores Rscript rxx031mpi.R --n.iter=10000 --dateStr=$DATE --guideFl=FullGuide.RData --estFlNm=$ESTFL --group=$taskId
